d67579d6c65c9899e7b682a07fa39c2cdad8dd10
Andrew Donaldson <andrew@desto.net>
2010-06-26 10:28:04 +0100

    Updated readme for github push

--- test/test_sync_outwards.js
+++ test/test_sync_outwards.js
@@ -27,11 +27,13 @@ test("will execute multiple callbacks when sync_out'ing", function() {
   ok(animal_name_out.attr('class') == this.host.animal_name, "second callback failed to set value")
 });
 
-test("will allow the user to manipulate the array", function() {
+test("will allow the user to retrieve an array of the callbacks", function() {
   this.host.sinko.outs('animal_name', function() { $("#animal_name_out").html(this.animal_name) }); 
   this.host.sinko.outs('animal_name', function() { $("#animal_name_out").attr("class", this.animal_name) }); 
 
   var outs = this.host.sinko.outs('animal_name');
+
+  // This is because 'sinko' is a module rather than an instance -- need to fix that
   ok(outs.length == 2, "callback queue is not the correct length");
 });
 
